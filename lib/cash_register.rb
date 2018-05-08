5ea4c5b9c5bdd94341074e5b689f4558e4ce8322
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-05-08 21:56:52 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
     
   def add_item(title, price, quantity = 1)
     self.total += price*quantity
-    self.items.clear
+
     i = 1
     while i <= quantity do
      @@items << title
