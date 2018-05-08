30148b2bd796c77ec66dc169bc021ce73029094c
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-05-08 21:02:29 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
     
   def add_item(title, price, quantity = 1)
     self.total += price*quantity
-    self.items << title
+    self.items << title if quanity > 1
   end
   
   def apply_discount
