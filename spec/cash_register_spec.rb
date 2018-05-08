e35c819b64b2cf618de710029930f7b24b03fd57
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-05-08 22:47:04 +0000

    automatically backed up by learn

--- spec/cash_register_spec.rb
+++ spec/cash_register_spec.rb
@@ -68,7 +68,6 @@ describe 'CashRegister' do
   describe '#items' do
     it 'returns an array containing all items that have been added' do
       new_register = CashRegister.new
-      new_register.items.clear
       new_register.add_item("eggs", 1.99)
       new_register.add_item("tomato", 1.76, 3)
       expect(new_register.items).to eq(["eggs", "tomato", "tomato", "tomato"])
